945e9b9f68fe057542ecbb21e831e6c9976664c0
Thomas Hauser <thomas.hauser@colorado.edu>
2014-09-02 16:28:29 -0600

    Upcased keywords in triad.f90

--- HW01/triad.f90
+++ HW01/triad.f90
@@ -2,91 +2,91 @@
 ! triad.f90 benchmark demo code
 ! G. Hager, 2010
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-program triad
-  implicit none
+PROGRAM triad
+  IMPLICIT NONE
 
-  double precision, dimension(:),allocatable :: A,B,C,D
+  DOUBLE PRECISION, DIMENSION(:),ALLOCATABLE :: A,B,C,D
 ! Intel-specific: 16-byte alignment of allocatables
 !DEC$ ATTRIBUTES ALIGN: 16 :: A
 !DEC$ ATTRIBUTES ALIGN: 16 :: B
 !DEC$ ATTRIBUTES ALIGN: 16 :: C
 !DEC$ ATTRIBUTES ALIGN: 16 :: D
-  double precision :: MFLOPS,WT
-  integer :: N,i
-  integer(kind=8) :: R
+  DOUBLE PRECISION :: MFLOPS,WT
+  INTEGER :: N,i
+  INTEGER(kind=8) :: R
 
-  read *,N
+  READ *,N
 
-  allocate(A(1:N),B(1:N),C(1:N),D(1:N))
+  ALLOCATE(A(1:N),B(1:N),C(1:N),D(1:N))
 
-  do i=1,N
+  DO i=1,N
      A(i) = 0.d0; B(i) = 1.d0
      C(i) = 2.d0; D(i) = 3.d0
-  enddo
+  ENDDO
 
   R = 1
 
   ! warm up
-  call do_triad(A,B,C,D,N,R,WT)
+  CALL do_triad(A,B,C,D,N,R,WT)
 
-  do
-     call do_triad(A,B,C,D,N,R,WT)
+  DO
+     CALL do_triad(A,B,C,D,N,R,WT)
      ! exit if duration was above some limit
-     if(WT.ge.0.2d0) exit
+     IF(WT.GE.0.2d0) EXIT
      ! else do it again with doubled repeat count
      R = R*2
-  enddo
+  ENDDO
 
   MFLOPS = R*N*2.d0/(WT*1.d6) ! compute MFlop/sec rate
-  print *, "Length: ",N,"   MFLOP/s: ",MFLOPS
-  deallocate(A,B,C,D)
-end program triad
+  PRINT *, "Length: ",N,"   MFLOP/s: ",MFLOPS
+  DEALLOCATE(A,B,C,D)
+END PROGRAM triad
 
-subroutine do_triad(A,B,C,D,N,R,WT)
-  implicit none
-  integer, intent(in) :: N
-  integer(kind=8), intent(in) :: R
-  double precision, dimension(N), intent(out) :: A
-  double precision, dimension(N), intent(in) :: B,C,D
-  double precision, intent(out) :: WT
-  double precision :: S,E
-  integer :: N2
+SUBROUTINE do_triad(A,B,C,D,N,R,WT)
+  IMPLICIT NONE
+  INTEGER, INTENT(in) :: N
+  INTEGER(kind=8), INTENT(in) :: R
+  DOUBLE PRECISION, DIMENSION(N), INTENT(out) :: A
+  DOUBLE PRECISION, DIMENSION(N), INTENT(in) :: B,C,D
+  DOUBLE PRECISION, INTENT(out) :: WT
+  DOUBLE PRECISION :: S,E
+  INTEGER :: N2
   ! assume 4MB outer level cache
-  integer, parameter :: CACHE_LIMIT=131072
-  integer :: i
-  integer(kind=8) :: j
+  INTEGER, PARAMETER :: CACHE_LIMIT=131072
+  INTEGER :: i
+  INTEGER(kind=8) :: j
 
   N2 = N/2
 
-  call get_walltime(S)
+  CALL get_walltime(S)
   
-  if(N.le.CACHE_LIMIT) then
-     do j=1,R
+  IF(N.LE.CACHE_LIMIT) THEN
+     DO j=1,R
 ! Intel-specific: Assume aligned moves
 !DEC$ vector aligned
 !DEC$ vector temporal
-        do i=1,N
+        DO i=1,N
            A(i) = B(i) + C(i) * D(i)
-        enddo
+        ENDDO
         ! prevent loop interchange
-        if(A(N2).lt.0) call dummy(A,B,C,D)
-     enddo
-  else
-     do j=1,R
+        IF(A(N2).LT.0) CALL dummy(A,B,C,D)
+     ENDDO
+  ELSE
+     DO j=1,R
 ! Intel-specific: Assume aligned moves
 !DEC$ vector aligned
 !DEC$ vector nontemporal
-        do i=1,N
+        DO i=1,N
            A(i) = B(i) + C(i) * D(i)
-        enddo
+        ENDDO
         ! prevent loop interchange
-        if(A(N2).lt.0) call dummy(A,B,C,D)
-     enddo
-  endif
+        IF(A(N2).LT.0) CALL dummy(A,B,C,D)
+     ENDDO
+  ENDIF
   
-  call get_walltime(E)
+  CALL get_walltime(E)
   
   WT = E-S
 
-end subroutine do_triad
+END SUBROUTINE do_triad
 
